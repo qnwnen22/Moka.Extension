2db38614cd3f08bc371717d5fcae1c48da692129
원혁 이 <qnwnen0228@gmail.com>
2024-01-09 20:00:28 +0900

    JsonExtension 내용추가

--- Moka.Extension/JsonExtension.cs
+++ Moka.Extension/JsonExtension.cs
@@ -1,9 +1,28 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 
 namespace Moka.Extension
 {
     public static class JsonExtension
     {
+        public static JToken GetToken(this JObject jObject, params string[] keys)
+        {
+            JToken jToken = null;
+            foreach (string key in keys)
+            {
+                bool isTry = jObject.TryGetValue(key, out jToken);
+                if (isTry == false) throw new Exception($"[{key}]로 이루어진 JToken을 찾을 수 없습니다.");
+            }
+            return jToken;
+        }
+
+        public static JToken GetToken(this JToken jToken, params string[] keys)
+        {
+            JObject jObject = jToken.ToObject<JObject>();
+            return jObject.GetToken(keys);
+        }
+
         public static string ToJson(this object value, Formatting formatting = Formatting.None)
         {
             return JsonConvert.SerializeObject(value, formatting);
